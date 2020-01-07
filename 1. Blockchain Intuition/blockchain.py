6f628e59a0738ecf0f6a75c9facc60a80d76c6ef
vijayrawatgithub <30477642+vijayrawatgithub@users.noreply.github.com>
2020-01-08 01:05:15 +0530

    Update blockchain.py

--- 1. Blockchain Intuition/blockchain.py
+++ 1. Blockchain Intuition/blockchain.py
@@ -1,9 +1,4 @@
-# Module 1 - Create a Blockchain
-
-# To be installed:
-# Flask==0.12.2: pip install Flask==0.12.2
-# Postman HTTP Client: https://www.getpostman.com/
-
+# Create a Blockchain
 # Importing the libraries
 import datetime
 import hashlib
